8d496c2891e55fcfe2c27d6838d867afb6baed88
tjaszai <tjaszai.dev@proton.me>
2024-06-05 00:53:03 +0200

    refactor purge.sh file

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-# Remove all docker containers with name starting with 'service-hub-'
-docker rm $(docker stop $(docker ps -a --filter name="service-hub-" --format="{{.ID}}"))
+name_prefix="service-hub-"
 
-# Remove unused docker images
-docker image prune -a -f
+# Filter docker containers, volumes and images by name prefix
+containers_and_images=$(docker ps -a --filter name="$name_prefix" --format="{{.ID}};{{.Image}}")
+volumes=$(docker volume ls -q --filter name="$name_prefix")
 
-# Remove unused docker volumes
-docker volume prune -a -f
+for container_and_image in $(echo "$containers_and_images"); do
+    elements=($(echo "$container_and_image" | tr ';' ' '))
+
+    container="${elements[0]}"
+    image="${elements[1]}"
+
+    # Remove all docker containers with name starting with 'service-hub-'
+    docker rm $(docker stop "$container") --volumes
+
+    # If possible, remove the unused docking image ( service hub related )
+    docker rmi "$image"
+done
+
+# If possible, remove the unused docking volume ( service hub related )
+for volume in $(echo "$volumes"); do
+    docker volume rm "$volume"
+done
